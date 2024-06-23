aeef0962a3d8b11983d154efafa541a4cb17d855
AlessioW3bno <alessio@w3bno.it>
2024-06-23 03:31:04 +0200

    first commit

---
+++ popup.js
@@ -0,0 +1,43 @@
+// popup.js
+
+document.getElementById("extract").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: extractData,
+    });
+  });
+});
+
+// Definire la funzione di estrazione dati qui
+function extractData() {
+  let table = document.querySelector(
+    "#mainContent ui-view ta-test-case-list-route test-cases responsive-table table > tbody"
+  );
+
+  let childs = table.querySelectorAll("tr:not(:first-child)");
+
+  let sum = 0;
+
+  Array.from(childs, (element) => {
+    let result = element.querySelector(
+      'td[data-column-title="Result "] > span'
+    ).innerHTML;
+    if (result.includes("Unclaimed")) return;
+
+    result = element.querySelector(
+      'td[data-column-title="Approval "] > span'
+    ).innerHTML;
+    if (result.includes("Approved")) return;
+
+    let payout = element
+      .querySelector('td[data-column-title="Payout "]')
+      .innerHTML.slice(1);
+
+    sum += parseFloat(payout);
+
+    console.log(payout);
+  });
+
+  alert("Upcoming payout: "+ sum);
+}
